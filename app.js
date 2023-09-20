302646871c29e5c531ba6eb47e0aef1541b7a8ce
Anurag G.C. <anrggc7@gmail.com>
2023-09-20 12:07:32 +0545

    crud

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ require("./model/index");
 
 app.use(express.static("public/"));
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,14 +16,17 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/ourTeams", (req, res) => {
-  res.render("ourTeams.ejs");
+app.get("/ourTeams", async (req, res) => {
+  const teams = await employee.findAll();
+  console.log(teams);
+  res.render("ourTeams.ejs", { teams: teams });
 });
 
 app.get("/addEmployee", (req, res) => {
   res.render("addemployee.ejs");
 });
 
+// Add to your Team
 app.post("/addEmployee", async (req, res) => {
   const name = req.body.name;
   const age = req.body.age;
@@ -40,6 +42,64 @@ app.post("/addEmployee", async (req, res) => {
   });
 
   res.redirect("/");
+});
+
+// Member Details
+app.get("/single/:id", async (req, res) => {
+  const id = req.params.id;
+  const team = await employee.findAll({
+    where: {
+      id: id,
+    },
+  });
+  res.render("team.ejs", { team: team });
+});
+
+//Delete Record
+app.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await employee.destroy({
+    where: {
+      id: id,
+    },
+  });
+  res.redirect("/");
+});
+
+//Edit Record
+app.get("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const team = await employee.findAll({
+    where: {
+      id: id,
+    },
+  });
+  res.render("update.ejs", { team: team });
+});
+
+app.post("/updaterecord/:id", async (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const age = req.body.age;
+  const designation = req.body.employeepost;
+  const EmployeeID = req.body.EmployeeID;
+  console.log(req.body);
+
+  await employee.update(
+    {
+      name: name,
+      age: age,
+      designation: designation,
+      employeeid: EmployeeID,
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  );
+
+  res.redirect("/single/" + id);
 });
 
 app.listen(3000, () => {
